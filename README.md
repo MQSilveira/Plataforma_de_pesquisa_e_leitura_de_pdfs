7a400820b64d913759cb8c1478a57e2cdbd33387
Marcos Silveira <marcosilveira.lg@gmail.com>
2023-03-01 11:03:30 -0300

    envio do projeto finalizado

---
+++ README.md
@@ -0,0 +1,48 @@
+# APLICAÇÃO DESENVOLVIDA E APRESENTADA EM SALA DE AULA
+<br>
+<h2 align="center"> :books: Biblioteca Django :books: </h2>
+
+<p align="center">
+  <img src="https://gifs.eco.br/wp-content/uploads/2022/10/gifs-de-leitura-6.gif" alt="Uma pessoa passando os dedos por vários livros" />
+</p>
+<p align="center">
+    <a href="#">
+      <img src="https://img.shields.io/badge/Status-Finalizado-brightgreen" alt="Badge Finalizado"/>
+    </a>
+    <a href="https://github.com/MQSilveira/aplicacao_django_moredevs2blu/blob/main/LICENSE">
+      <img src="https://img.shields.io/badge/Licen%C3%A7a-MIT-brightgreen" alt="Badge Licença MIT"/>
+    </a>
+    <a href="#">
+    <img src="https://img.shields.io/badge/Vers%C3%A3o%20Final-Janeiro%2F23-blue" alt="Badge Versão Final"/>
+    </a>
+</p>
+<br>
+<br>
+<h3 align="center">Objetivo do Projeto</h3>
+<p>O presente projeto visa fornecer um meio digital que seja prático para pesquisa e leitura de PDF's para os usuários. 
+<br>
+Por meio de pesquisa e lista de recém adicionados na página inicial, o usuário procura o livro que gostaria de ler, e o site redireciona para aquele que foi solicitado ou selecionado.</p>
+<br>
+<br>
+<h3 align="center">O que foi Utilizado no Projeto</h3>
+<ul>
+    <li>Django 4.1.7</li>
+    <li>Python 3.11.0</li>
+    <li>PostgreSQL </li>
+    <li>Docker Desktop 4.12.0</li>
+    <li>HTML5</li>
+    <li>CSS3</li>
+    <li>Jira Software</li>
+</ul>
+<br>
+<br>
+<h3 align="center">Autores</h3>
+<p>
+  <a href="https://github.com/MQSilveira">Marcos Silveira</a>
+</p>
+<p>
+  <a href="https://github.com/lsebold">Larissa Sebold</a>
+</p>
+<p>
+  <a href="https://github.com/marciolou">Márcio Lourenço</a>
+</p>
---
+++ biblioteca/settings.py
@@ -0,0 +1,130 @@
+from pathlib import Path
+import os
+
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-sdu_9ed@_eswwo#3%mg_+1v1(7b04y#(e7n%dt^ogix5^ra)_y'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'livros.apps.LivrosConfig',
+    'ckeditor',                 # Permite editar texto de maneira "rica" (negrito, italico, fontes, ...)
+    'ckeditor_uploader',        # Uploader de imagens para o texto
+    'usuarios.apps.UsuariosConfig',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'biblioteca.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],       # os.path.join()    concatena os diretórios (BASE_DIR + templates)
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'biblioteca.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'moredevs',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '5436',                                 
+    }
+}
+
+# Password validation
+# https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/4.1/topics/i18n/
+
+LANGUAGE_CODE = 'pt-br'
+
+TIME_ZONE = 'America/Sao_Paulo'
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.1/howto/static-files/
+
+STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# Estamos dizendo que o caminho absoluto para onde nossas Imagens, 
+# CSS e JavaScript serão armazenados é no diretório BASE_DIR / static Files (STATIC_ROOT)
+
+CKEDITOR_UPLOAD_PATH = 'uploads/'
+
+MEDIA_URL = 'images/'           # Da um caminho relativo para onde ficam os arquivos que chegam em nossa aplicação por meio de um upload.
+MEDIA_ROOT = os.path.join(BASE_DIR, MEDIA_URL)        # Informar o caminho absoluto de onde encontram as imagens que chegam em nossa aplicação por meio de um upload.
+
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
